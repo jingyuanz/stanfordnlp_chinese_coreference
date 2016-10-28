7aae645c056915961714605639b704c0c03ba99f
jingyuanz <barrel.titor.daru@gmail.com>
2016-10-28 15:37:48 +0800

    correct_coref_version

--- Nlp.java
+++ Nlp.java
@@ -1,65 +1,40 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+package test;
+
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
-import edu.stanford.nlp.io.IOUtils;
+import java.util.Map;
+import java.util.Properties;
+import edu.stanford.nlp.hcoref.CorefCoreAnnotations;
+import edu.stanford.nlp.hcoref.data.CorefChain;
+import edu.stanford.nlp.hcoref.data.Mention;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.PropertiesUtils;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.StringUtils;
 public class Nlp {
-	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
-//	    String props="test";
-//	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-		StanfordCoreNLP pipeline = new StanfordCoreNLP(
-				PropertiesUtils.asProperties(
-					"annotators", "segment, ssplit, pos, ner, parse, mention, coref",
-					"customAnnotatorClass.segment", "edu.stanford.nlp.pipeline.ChineseSegmenterAnnotator",
-					"segment.model","edu/stanford/nlp/models/segmenter/chinese/ctb.gz",
-					"segment.sighanCorporaDict","edu/stanford/nlp/models/segmenter/chinese",
-					"segment.serDictionary","edu/stanford/nlp/models/segmenter/chinese/dict-chris6.ser.gz",
-					"segment.sighanPostProcessing","true",
-					"ssplit.boundaryTokenRegex","[.]|[!?]+|[。]|[！？]+",
-					"pos.model","edu/stanford/nlp/models/pos-tagger/chinese-distsim/chinese-distsim.tagger",
-					"ner.model","edu/stanford/nlp/models/ner/chinese.misc.distsim.crf.ser.gz",
-					"ner.applyNumericClassifiers","false",
-					"ner.useSUTime","false",
-					"parse.originalDependencies","true",
-					"parse.model", "edu/stanford/nlp/models/lexparser/chineseFactored.ser.gz"
-					));
-	    Annotation annotation;
-	    //if  data from file
-	    File f = new File("/home/jingyuanz/Desktop/work/new/src/input.txt");
-	    BufferedReader reader = null;
-	    reader = new BufferedReader(new FileReader(f));
-	    String tempString = reader.readLine();
-	    String next = null;
-	    long sta=System.currentTimeMillis();
-	    int i = 0;
-	    while ((next = reader.readLine()) != null) {
-	    	String text = tempString + next;
-	    	tempString = next;
-	    	annotation = new Annotation(text);
-	    	long starTime=System.currentTimeMillis();
-	    	pipeline.annotate(annotation);
-	    	long endTime=System.currentTimeMillis();
-	    	i++;
-	    	pipeline.prettyPrint(annotation, System.out);
-	    	System.out.println(i);
-	    	System.out.println("each time");
-	    	System.out.println(endTime - starTime);
-    	}
-	    long end=System.currentTimeMillis();
-	    System.out.println(sta - end);
-	    
-//	     annotation = new Annotation("这家酒店很好。它很漂亮，我很喜欢。");
-	    
-	    
-	    
-	    
-//	    pipeline.prettyPrint(annotation, System.out);
+		        String text = "斯坦福大学坐落在美国,它很大.小明吃棒冰,他很胖.";
+		        args = new String[] {"-props", "edu/stanford/nlp/hcoref/properties/zh-coref-default.properties"};
+		        //ssplit.boundaryTokenRegex=[.]|[!?]+|[。]|[！？]+
+		        
+		        Annotation document = new Annotation(text);
+		        Properties props = StringUtils.argsToProperties(args);
+		        props.replace("ssplit.boundaryTokenRegex", "[.]|[!?]+|[。]|[！？]+", "[.,]|[!?]+|[。，]|[！？]+");
+		        System.out.println(args);
+		        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		        System.out.println(props);
+		        for (int i=0;i<10;i++){
+		        	long startTime=System.currentTimeMillis();
+			        pipeline.annotate(document);
+//			        pipeline.prettyPrint(document, System.out);
+	
+			        for (CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
+			            System.out.println("\t" + cc);
+			        }
+	
+			        long endTime=System.currentTimeMillis(); 
+			        long time = (endTime-startTime);
+			        System.out.println(time);
+		        }
 	}
 }
